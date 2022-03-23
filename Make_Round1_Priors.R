91ec0d3a7193bec235400dcea57dc3c332f58c5f
rebeccamancy <rebecca.mancy@gmail.com>
2022-03-23 12:04:56 +0000

    Tidied code

--- Make_Round1_Priors.R
+++ Make_Round1_Priors.R
@@ -13,26 +13,6 @@ for (rr in 1:n.priors) {
 hist(my.priors$ThShape)
 hist(my.priors$ThScale)
 write.table(my.priors, file = "output/Round1_priors.csv", append = FALSE, quote = FALSE, sep = ",", eol = "\n", na = "NA", dec = ".", row.names = FALSE, col.names = TRUE, fileEncoding = "")
-#write.table(my.priors, file = paste0("../data_transferred/sampling_params_",model,".csv"), append = FALSE, quote = FALSE, sep = ",", eol = "\n", na = "NA", dec = ".", row.names = FALSE, col.names = TRUE, fileEncoding = "")
 summary(my.priors$ThShape*my.priors$ThScale) # Mean handling time
 
-# # Check that it all worked OK
-# test <- read.csv(file = "~/Documents/large_data/v04/infData_fixedDD_1000/output_files/infData_fixedDD_1000_sim_cases_001_001.csv")
-# hist(test$Th, freq = F); mean(test$Th)
-# ThScale=2.42325876135146
-# ThShape=5.5735694821924
-# hist(rgamma(10000, shape = ThShape, scale = ThScale), border="red", add=T, freq = F)
-# 
-# test <- read.csv(file = "~/Documents/large_data/v04/infData_fixedDD_1000/output_files/infData_fixedDD_1000_sim_cases_002_001.csv")
-# hist(test$Th, freq = F); mean(test$Th)
-# ThScale=9.5999389491044
-# ThShape=0.945977188763209
-# hist(rgamma(10000, shape = ThShape, scale = ThScale), border="red", add=T, freq = F)
-# min(test$Th)
-# 
-# test <- read.csv(file = "~/Documents/large_data/v04/infData_fixedDD_1000/output_files/infData_fixedDD_1000_sim_cases_006_001.csv")
-# hist(test$Th, freq = F); mean(test$Th)
-# ThScale=7.21694943943294
-# ThShape=0.52040371124167
-# hist(rgamma(10000, shape = ThShape, scale = ThScale), border="red", add=T, freq = F)
-# min(test$Th)
+
